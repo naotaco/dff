748f6a26dac06ffc1fbeda65b029080a6edc6229
Nao Taco <naotaco@gmail.com>
2019-01-26 21:57:47 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,40 @@ fn main() -> Result<(), std::io::Error>{
     	None => std::env::current_dir()?,
     };
 
-    let mut filenames = HashMap::new();
+    let mut filenames : HashMap<String, Vec<std::path::PathBuf>> = HashMap::new();
 
     for entry in WalkDir::new(root.as_path())
             .into_iter()
             .filter_map(Result::ok)
             .filter(|e| !e.file_type().is_dir()) {
         let f_name = String::from(entry.file_name().to_string_lossy());
-        let counter = filenames.entry(f_name.clone()).or_insert(0);
-        *counter += 1;
 
-        if *counter > 1{
-	        println!("{:?} : {:?}", *counter, f_name);
-	    }
+        // find similar one
+        for k in filenames.keys(){
+        	if is_similar(&f_name, &k.to_string()){
+        		let mut list = match filenames.get(k){
+        			Some(l) => l,
+        			None => {
+	        			},
+        		};
+        		list.push(entry.into_path());
+        	}else{
+        		// filenames.insert(f_name, Vec::new());
+        	}
+        }
+
+    }
+
+    for k in filenames.keys(){
+    	match filenames.get(k) {
+    	 	Some(path_list) => println!("found: {:?}", path_list.iter().count()),
+    	 	None => continue,
+    	 } 
 
     }
     Ok(())
 }
 
-
+fn is_similar(a:&String, b:&String) -> bool {
+	true
+}
