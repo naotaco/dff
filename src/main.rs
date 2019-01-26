10bd9533f222df0a60fd647028b4aec2f9413c70
Nao Taco <naotaco@gmail.com>
2019-01-26 17:01:57 +0900

    count

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+extern crate walkdir;
+
+use std::collections::HashMap;
+use walkdir::WalkDir;
+
+fn main() -> Result<(), std::io::Error>{
+    let mut args: Vec<String> = std::env::args().collect();
+    let root = match args.pop() {
+    	Some(p) => std::path::PathBuf::from(p),
+    	None => std::env::current_dir()?,
+    };
+
+    let mut filenames = HashMap::new();
+
+    for entry in WalkDir::new(root.as_path())
+            .into_iter()
+            .filter_map(Result::ok)
+            .filter(|e| !e.file_type().is_dir()) {
+        let f_name = String::from(entry.file_name().to_string_lossy());
+        let counter = filenames.entry(f_name.clone()).or_insert(0);
+        *counter += 1;
+
+        if *counter > 1{
+	        println!("{:?} : {:?}", *counter, f_name);
+	    }
+
+    }
+    Ok(())
 }
+
+
